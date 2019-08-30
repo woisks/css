4804611d6d5ba7c4da79875dc4987cfa5d09f039
woisks <woisks@126.com>
2019-08-30 22:57:31 +0800

    'dev3'

--- src/component/home-page/index.js
+++ src/component/home-page/index.js
@@ -2,44 +2,21 @@ import React from 'react';
 
 
 const Index = () => {
+
     return (
         <div className='container'>
 
-            <hr/>
-
-            <div className="columns is-align-end is-wrap">
-                <div className="column is-self-start text">
-                    <a className="ant-btn " href="/login">start</a>
-
-                </div>
-
-
-                <div className="column">
-                    <a className="ant-btn " href="/login">center</a>
-
-                </div>
-
-                <div className="column is-6">
-
-                    <a className="ant-btn " href="/login">end</a>
-
-                </div>
-                <div className="column ">
-
-                    <a className="ant-btn " href="/login">end</a>
-
-                </div>
-                <div className="column ">
-
-                    <a className="ant-btn " href="/login">end</a>
-
-                </div>
-                <div className="column ">
-
-                    <a className="ant-btn " href="/login">end</a>
-
-                </div>
+            {/*<div className="flex">*/}
+            {/*<div className="auto">*/}
+            <div className="margin-auto">
+                <figure className="image is-256x256  ">
+                    <img src="http://woisk.cn/logo.png" image="da" alt=""
+                         className="is-rounded animate-rotate home-logo"/>
+                </figure>
             </div>
+            {/*</div>*/}
+            {/*</div>*/}
+
 
         </div>
 
--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {withFormik} from 'formik';
-import {Button, Col, Form, Input, Row} from "antd";
+import {Button, Form, Input} from "antd";
 import * as Yup from 'yup';
 import {Link} from "react-router-dom";
 import logo from '../../logo.png'
@@ -17,7 +17,7 @@ const registerForm = props => {
         <div className='container'>
 
             <div className='align-center'>
-                <img className='form-logo' src={logo} onClick={logoClick} alt="Woisk"/>
+                <img className='form-logo is-pointer' src={logo} onClick={logoClick} alt="Woisk"/>
             </div>
             <div className="form">
                 <Form onSubmit={handleSubmit} className="login-form">
@@ -33,6 +33,7 @@ const registerForm = props => {
                             id="username"
                             size='large'
                             placeholder='UID/邮箱/登录名/手机号'
+
                         />
                     </Form.Item>
 
@@ -51,10 +52,12 @@ const registerForm = props => {
                             type='password'
                             placeholder='密码'
                         />
+
                     </Form.Item>
 
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" size='large' block disabled={isSubmitting}>登 录</Button>
+                        <Button type="primary" htmlType="submit" size='large' block
+                                disabled={isSubmitting}>登 录</Button>
                     </Form.Item>
                     <Form.Item>
 
@@ -77,7 +80,6 @@ const registerForm = props => {
 const Register = withFormik({
     mapPropsToValues: () => ({
         username: '',
-
         password: ''
     }),
 
